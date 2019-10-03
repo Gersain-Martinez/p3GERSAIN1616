ecbf921f2356557c2d760d5a4a4179f38dfc3f14
Gersian-Martinez <martinezgersain30@gmail.com>
2019-10-03 13:46:23 -0500

    Initial commit

---
+++ app/src/main/java/com/gmh/itics/tesoem/edu/p3gersain1616/FrmPrincipalActivity.java
@@ -0,0 +1,14 @@
+package com.gmh.itics.tesoem.edu.p3gersain1616;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FrmPrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frm_principal);
+    }
+}
---
+++ app/src/main/java/com/gmh/itics/tesoem/edu/p3gersain1616/MainActivity.java
@@ -0,0 +1,24 @@
+package com.gmh.itics.tesoem.edu.p3gersain1616;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent principal = new Intent(MainActivity.this, FrmPrincipalActivity.class);
+                startActivity(principal);
+                finish();
+            }
+        }, 4000);
+    }
+}
